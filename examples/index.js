2f62f44fdd57d89dc964b2269851ab59c2d98346
primenumsdev <code.primenums@gmail.com>
2021-10-16 15:31:15 +0300

    update readme and examples

--- examples/index.js
+++ examples/index.js
@@ -2,35 +2,44 @@ const NFTs = require('../lib');
 const web3 = require("@solana/web3.js");
 
 (async () => {
-  // Connect to Solana
+  
+  // Create connection
   const conn = new web3.Connection(
     web3.clusterApiUrl('mainnet-beta'),
     'confirmed'
   );
 
-  // This is your NFT mint address
-  // const mintAddr = "8jbcPTiZEU42XVYpkx1XLDgRPXyMy9HiN3KTtpFq1uPX";
-  
-  // const myNFT = await NFTs.getNFTByMintAddress(conn, mintAddr);
-  // console.log('myNFT', JSON.stringify(myNFT, null, 2));
+  // Get all mint tokens (NFTs) from your wallet
+  const walletAddr = 'EaeLkUWHDXBRcLfvBXhczgavxPtCBASYYXB9rBrYN1b6';
+  let mints = await NFTs.getMintTokensByOwner(conn, walletAddr);
+  console.log('mints', mints);
 
-  // Get all NFTs in your wallet
-  // This wallet has 80k NFTs
-  // const walletAddr = 'GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp';
-  // const mints = await NFTs.getMintTokensByOwner(conn, walletAddr);
-  // console.log('tota mints', mints.length);
+  // Now we can get some NFT information (metadata)
+  let myNFT = await NFTs.getNFTByMintAddress(conn, mints[0]);
+  console.log('myNFT', myNFT);
 
-  // "HfaFdRYzfzyf1RaZo48ueDJSe2oP44Sm6nid24n3Awf",
-  // "6TNBbACaSBwDXCwrEfS73u2ebTyfC23PAcwaXc7o7Lax",
-  // "BQ55YZoCuDb8yVu9aRnm8VXK4Ao1WNYLXZDw1NRQzavT",
-  // "EfDGMigJ3UbKGf3VCwToURqfsZFhCkKYeJjZ7WWfAdsU",
-  // const someNft = await NFTs.getNFTByMintAddress(conn, 'EfDGMigJ3UbKGf3VCwToURqfsZFhCkKYeJjZ7WWfAdsU');
-  // console.log('someNft', someNft);
+  // Additional function to get all NFTs from wallet address
+  let allMyNFTs = await NFTs.getNFTsByOwner(conn, walletAddr);
+  console.log('allMyNFTs', allMyNFTs);
 
+  // Be aware that some wallets may have too many NFTs, and Solana API may throw 429 Too many requests error.
 
-  // Can do paginated NFTs retrieval
-  const page1 = await NFTs.getNFTsByOwner(conn, 'GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp', 1, 1, 0.5);
-  console.log('page 1', page1);
-  const page2 = await NFTs.getNFTsByOwner(conn, 'GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp', 2, 1, 0.5);
-  console.log('page 2', page2);
+  // Solana cluster rate limits:
+  // https://docs.solana.com/cluster/rpc-endpoints#rate-limits-2
+
+  // To handle this, you can first query all mint tokens, and then sequentially get NFT data for each.
+  mints = await NFTs.getMintTokensByOwner(conn, walletAddr);
+  myNFT = await NFTs.getNFTByMintAddress(conn, mints[0]);
+  console.log('myNFT', myNFT);
+
+  // Or use this function with pagination and caching
+  let page = 1;
+  const perPage = 5;
+  const cacheTtlMins = 0.1; // Will keep the mint tokens cached for 6 seconds
+  let myNFTsPage1 = await NFTs.getNFTsByOwner(conn, walletAddr, page, perPage, cacheTtlMins);
+  console.log('myNFTsPage1', myNFTsPage1);
+
+  page++;
+  let myNFTsPage2 = await NFTs.getNFTsByOwner(conn, walletAddr, page, perPage, cacheTtlMins);
+  console.log('myNFTsPage2', myNFTsPage2);
 })();
